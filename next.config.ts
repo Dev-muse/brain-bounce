c9fea7ad9202764da802264a76da4168019ac6e9
Rahman <museabdirahman@gmail.com>
2025-12-20 21:28:48 +0000

    white list production url hostname to allow images to be loaded from convex

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "standing-oriole-929.convex.cloud",
+        hostname: "admired-elephant-261.convex.cloud",
       },
     ],
   },
